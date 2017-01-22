2ea46c89ed99680d40133a86845196888cd11bbf
Elvis <elvis.perazic@gmail.com>
2017-01-22 17:59:55 +0100

    Ajout de Savior et de Solution + Style tunnel d'inscription

---
+++ savior.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tripper</title>
+    <link rel="stylesheet" type="text/css" href="css/style_savior.css" />
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+    <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <meta name="description" content="Vous cherchez à vous procurer de la liquidité. Quel qu’en soit la raison, pas de banque à proximité, perte de ses moyens de paiement, Tripper vous sauve la mise avec ses Saviors.">
+    <!-- jQuery library (served from Google) -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <!-- bxSlider Javascript file -->
+    <script src="js/jquery.bxslider.min.js"></script>
+    <!-- bxSlider CSS file -->
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="http://www.tripper-app.com">
+    <meta name="twitter:creator" content="@trippersolution">
+    <meta name="twitter:title" content="Tripper">
+    <meta name="twitter:description" content="Nous mettons à votre disposition des Saviors à proximité qui répondront présent pour vous procurer de la liquidité.">
+    <meta name="twitter:image" content="http://www.tripper-app.com/img/image-twitter.png">
+</head>
+
+ <body>
+  <div class="header">
+    <div class="wrap">
+      <header>
+        <h1 class="logo">
+          <a href="">
+            <img src="img/logo-tripper.png" alt="TRIPPER">
+          </a>
+        </h1>
+      </header>
+      <nav>
+        <ul class="main-nav">
+          <li><a href="#" class="is-active">La solution</a></li>
+          <li><a href="#">Devenir savior</a></li>
+          <li><a href="#">Sécurité</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Contact</a></li>
+
+
+          <li><a href="">Login</a></li>
+          <li class="button-inscription"><a href=""> Inscription</a></li>
+        </ul>
+      </nav>
+      <button id="btn-nav">Menu</button>
+    </div>
+  </div>
+
+
+  <section class="first_container page-section">
+    <div class="container-fluid">
+     <div class="row">
+      <div class="col-md-8 col-md-offset-2 col-sm-12">
+        <h1>Profitez d'avantages en prêtant<br>main forte à nos <span style="color: #88B9E5;">Tripper</span></h1>
+        <p> Si Tripper fonctionne aujourd'hui, <br>c'est grâce à vous et aussi un peu à nous !</p><br>
+        <button>Devenir Savior</button>
+      </div>
+    </div>
+  </div>
+  <a href="#dernieres-nouvelles" ><img src="img/top_arrow.png" style="position: absolute; top: 778px; left: 47%;" alt=""></a>
+</section>
+
+<section class="second_container page-section" id="dernieres-nouvelles">
+  <div class="container">
+    <div class="row">
+      <h1>Tirez votre épingle du jeu !</h1>
+      <p>En toute sécurité.</p>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-md-4 col-xs-12">
+        <div class="blocbleu1">
+          <br><br><br>
+          <h3>Prenez contact</h3>
+          <br>
+          <img src="img/phone-new.png" class="img-responsive" width="160" alt="">
+        </div>
+      </div>
+      <div class="col-md-4 col-xs-12">
+        <div class="blocbleu2">
+          <br><br><br>
+        <h3>Effectuer la transaction</h3>
+        <br>
+        <img src="img/phone-new.png" class="img-responsive" width="160" alt="">
+        </div>
+      </div>
+      <div class="col-md-4 col-xs-12">
+        <div class="blocbleu3">
+          <br><br><br>
+          <h3>Récupérez votre Allopass</h3>
+          <br>
+          <img src="img/phone-new.png" class="img-responsive" width="160" alt="">
+        </div>
+        </div>
+    </div>
+  </div>
+</section>
+
+<section class="third_container page-section">
+  <div class="container-fluid">
+    <div class="row col-md-12">
+      <div class="col-md-1 col-xs-0"></div>
+      <div class="col-md-6 col-sm-12">
+        <div class="box__black">
+          <h2>Qu’est-ce qu’un Savior ?</h2>
+          <h3>Vous êtes le coeur de Tripper !</h3><br>
+          <p>Les Saviors (locaux ou commerces) partout dans le monde sont à votre disposition, prenez contact directement avec eux pour les rencontrer.
+          </p>
+        </div> 
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="four_container page-section">
+  <div class="container-fluid">
+    <div class="row">
+      <h1>Participez à l'aventure</h1>
+      <button>Devenir Savior</button>
+    </div>
+  </div>
+</section>
+
+
+
+<script src="js/script.js"></script>
+<script>
+  
+  $(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+</script>
+</body>
+</html>
---
+++ solution.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tripper</title>
+    <link rel="stylesheet" type="text/css" href="css/style_solution.css" />
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+    <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <meta name="description" content="Vous cherchez à vous procurer de la liquidité. Quel qu’en soit la raison, pas de banque à proximité, perte de ses moyens de paiement, Tripper vous sauve la mise avec ses Saviors.">
+    <!-- jQuery library (served from Google) -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <!-- bxSlider Javascript file -->
+    <script src="js/jquery.bxslider.min.js"></script>
+    <!-- bxSlider CSS file -->
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="http://www.tripper-app.com">
+    <meta name="twitter:creator" content="@trippersolution">
+    <meta name="twitter:title" content="Tripper">
+    <meta name="twitter:description" content="Nous mettons à votre disposition des Saviors à proximité qui répondront présent pour vous procurer de la liquidité.">
+    <meta name="twitter:image" content="http://www.tripper-app.com/img/image-twitter.png">
+</head>
+
+ <body>
+  <div class="header">
+    <div class="wrap">
+      <header>
+        <h1 class="logo">
+          <a href="">
+            <img src="img/logo-tripper.png" alt="TRIPPER">
+          </a>
+        </h1>
+      </header>
+      <nav>
+        <ul class="main-nav">
+          <li><a href="#" class="is-active">La solution</a></li>
+          <li><a href="#">Devenir savior</a></li>
+          <li><a href="#">Sécurité</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Contact</a></li>
+
+
+          <li><a href="">Login</a></li>
+          <li class="button-inscription"><a href=""> Inscription</a></li>
+        </ul>
+      </nav>
+      <button id="btn-nav">Menu</button>
+    </div>
+  </div>
+
+
+  <section class="first_container page-section">
+    <div class="container-fluid">
+     <div class="row">
+      <div class="col-md-8 col-md-offset-2 col-sm-12">
+        <h1><span style="color: #88B9E5;">Tripper,</span> l'assurance de voyager sans imprévu.</h1>
+        <p> Avec ou sans smartphone, nous offrons<br> la possibilité à notre communauté 
+            de changer<br> de devise à l'étranger, sans carte de retrait<br> & en toute sécurité.</p><br>
+        <h3><u>Comment ça marche exactement ?</u></h3>
+      </div>
+    </div>
+  </div>
+  <a href="#dernieres-nouvelles" ><img src="img/top_arrow.png" style="position: absolute; top: 778px; left: 47%;" alt=""></a>
+</section>
+
+<section class="second_container page-section" id="dernieres-nouvelles">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-1">
+        
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <img src="img/1.png" style="float: left; margin-top: 12%;" width="100" alt="">
+        <div class="box__black">
+          <h2>Envoyez un SMS au 89 900</h2>
+          <h3>Plus besoin de la 4G pour subvenir à vos besoins</h3>
+          <p> Notre système d’échange communautaire est basé sur la 
+            transaction monétaire via SMS entre voyageurs et Saviors.
+            <br><br>
+            Envoyez les informations suivantes à Tripper:
+            <br><br>
+            #PAYS #VILLE #RUE #SOMME au 89 900 
+            <br><br>
+            Les sommes que le voyageur demande ne peut excéder un équivalent de 
+            300 euros (en monnaie locale). Le paiement s'effectue avec votre le 
+            compte en banque relié à votre ligne téléphonique</p>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="hover-background">
+          <p style="text-align:center; margin-top: 34%;"><img src="img/plus.png" alt=""></p>
+          <button>Etape 1</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="third_container page-section">
+  <div class="container-fluid">
+    <div class="row col-md-12">
+      <div class="col-md-5">
+        <div class="hover-background">
+          <p style="text-align:center; margin-top: 34%;"><img src="img/plus.png" alt=""></p>
+          <button>Etape 2</button>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <div class="box__black">
+          <h2>Plusieurs Saviors à proximité</h2>
+          <h3>Il y a toujours des Saviors non loin de votre position géographique</h3>
+          <p>Des Saviors (locaux ou commerces) partout dans le monde
+           sont à votre disposition. Lors de l’envoi du SMS, Tripper 
+           vous indique quels sont les Saviors les plus proches de vous.
+            Voir l’exemple en cliquant sur le “plus”
+          </p>
+        </div> 
+        <img src="img/2.png" style="float: right; margin-top: 8%;" width="100" alt="">
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+  </div>
+</section>
+
+<section class="four_container page-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-1">
+        
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <img src="img/3.png" style="float: left; margin-top: 12%;" width="100" alt="">
+        <div class="box__black">
+          <h2>Sélectionnez votre Savior</h2>
+          <h3>Vous recevez un listing de Saviors, à vous de choisir le votre !</h3>
+          <p>Avec son logiciel Tripper récupère les informations que les Saviors 
+            ont communiqué et vous les envoi. Contactez le Savior de votre choix, 
+            donnez lui les informations nécessaires à la transaction avec le montant 
+            dont vous avez besoin et il viendra vous dépanner.</p>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="hover-background">
+          <p style="text-align:center; margin-top: 34%;"><img src="img/plus.png" alt=""></p>
+          <button>Etape 3</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="five_container page-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="row col-md-12">
+      <div class="col-md-5">
+        <div class="hover-background">
+          <p style="text-align:center; margin-top: 34%;"><img src="img/plus.png" alt=""></p>
+          <button>Etape 4</button>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <div class="box__black">
+          <h2>Faites votre transaction en main propre</h2>
+          <h3>Rencontrez votre Savior et effectuez la transaction en toute sécurité</h3>
+          <p>Une commission de 5% est appliquée à toutes les transactions de 1 à 100 euros, 
+            et de 10% de 100 à 300 euros. La commission est prise directement par le Savior.<br><br>
+            Le Savior peut se déplacer le plus proche possible ou vous indiquer un lieu de 
+            rendez-vous public proche (un café par exemple).
+          </p>
+        </div> 
+        <img src="img/4.png" style="float: right; margin-top: 8%;" width="100" alt="">
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+    </div>
+  </div>
+</section>
+
+<section class="six_container page-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-1">
+      </div>
+      <div class="col-md-10 col-sm-12">
+        <img src="img/5.png" style="float: left; margin-top: 12%;" width="100" alt="">
+        <div class="box__black">
+          <h2>L’aventure continue</h2>
+          <h3>Plus d’inquiétude, Tripper est là</h3>
+          <p>Consultez la partie Sécurité et F.A.Q pour disposer de toutes les réponses 
+            à vos questions ou n’hésitez pas à nous contacter, votre question alimentera la F.A.Q . 
+            <br><br>
+            Vous pouvez également retrouvez notre actualité sur les réseaux sociaux ou recevoir la 
+            newsletter pour être à jour de nos développements. 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="seven_container page-section">
+  <div class="container-fluid">
+    <div class="row">
+      <h1><span>TRIPPER</span> vous accompagne.</h1><br><br>
+      <p><img src="img/play-button.png" alt=""></p>
+    </div>
+  </div>
+</section>
+
+<script src="js/script.js"></script>
+<script>
+  
+  $(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+</script>
+</body>
+</html>
